92c00f6e8dbfcd8fd18a81c5cf723409b7985e34
Sergey Sochienko <59665883+fenq2@users.noreply.github.com>
2020-05-04 19:49:41 +0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+    svg4everybody({});
+});
+
+if($('.reviews__slider').length>0){
+	$('.reviews__slider').slick({
+		infinite: true,
+		dots: true,
+		arrows: true,
+		centerMode: true,
+		variableWidth: true,
+		centerMode: true,
+		slidesToShow: 1,
+		nextArrow:'<button type="button" class="slick-next"><svg version="1.1" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve"><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"/></svg></button>',
+		prevArrow:'<button type="button" class="slick-prev"><svg version="1.1" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve"><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></svg></button>',
+		responsive: [{breakpoint: 768, settings: {}}]
+	});
+}
+$('.colors-catalog__list li').click(function() {
+    var tabName = $(this).attr('show-tab');
+    $(this).addClass('colors-catalog__item--active').siblings().removeClass('colors-catalog__item--active');
+    $(this).closest('.item-catalog__nav').find('.item-catalog__slider .' + tabName).addClass('slider-prev--active').siblings().removeClass('slider-prev--active');
+});
+
+$('.slider-prev').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    nextArrow:'<button type="button" class="slick-next"><svg version="1.1" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve"><path d="M360.731,229.075l-225.1-225.1c-5.3-5.3-13.8-5.3-19.1,0s-5.3,13.8,0,19.1l215.5,215.5l-215.5,215.5c-5.3,5.3-5.3,13.8,0,19.1c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-4l225.1-225.1C365.931,242.875,365.931,234.275,360.731,229.075z"/></svg></button>',
+	prevArrow:'<button type="button" class="slick-prev"><svg version="1.1" x="0px" y="0px" viewBox="0 0 477.175 477.175" style="enable-background:new 0 0 477.175 477.175;" xml:space="preserve"><path d="M145.188,238.575l215.5-215.5c5.3-5.3,5.3-13.8,0-19.1s-13.8-5.3-19.1,0l-225.1,225.1c-5.3,5.3-5.3,13.8,0,19.1l225.1,225c2.6,2.6,6.1,4,9.5,4s6.9-1.3,9.5-4c5.3-5.3,5.3-13.8,0-19.1L145.188,238.575z"/></svg></button>',
+    cssEase: 'linear'
+  });
+
+
+  
+
+// Полифилы
+
+// forEach IE 11
+if ('NodeList' in window && !NodeList.prototype.forEach) {
+    console.info('polyfill for IE11');
+    NodeList.prototype.forEach = function (callback, thisArg) {
+        thisArg = thisArg || window;
+        for (var i = 0; i < this.length; i++) {
+            callback.call(thisArg, this[i], i, this);
+        }
+    };
+}
+
+// closest IE 11
+(function () {
+    if (!Element.prototype.closest) {
+        Element.prototype.closest = function (css) {
+            var node = this;
+            while (node) {
+                if (node.matches(css)) return node;
+                else node = node.parentElement;
+            }
+            return null;
+        };
+    }
+})();
+
+// matches IE 11
+(function () {
+    if (!Element.prototype.matches) {
+        Element.prototype.matches = Element.prototype.matchesSelector ||
+            Element.prototype.webkitMatchesSelector ||
+            Element.prototype.mozMatchesSelector ||
+            Element.prototype.msMatchesSelector;
+    }
+})();
+
+//Array.form IE 11
+if (!Array.from) {
+    Array.from = function (object) {
+        'use strict';
+        return [].slice.call(object);
+    };
+}
